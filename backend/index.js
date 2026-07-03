1885efaad05221b8e7db6b64d3c8f5b7e966c74e
shivang-dadhich <shivangdadhich344@gmail.com>
2026-07-03 23:51:41 +0530

    fix: comment out specific CORS origin configuration in middleware

--- backend/index.js
+++ backend/index.js
@@ -12,6 +12,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
+// app.use(cors({ origin: "https://pipeline-pilot-frontend.vercel.app/" })); 
 app.use(express.json());
 
 // MongoDB Connection
